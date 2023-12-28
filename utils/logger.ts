9d047b02e0c4a8f5364844f817b08b1ef36f0745
すえ <souhait.0614@gmail.com>
2023-12-28 19:04:10 +0900

    appNameをpublicに変更 logLevelをclass内に移動

--- utils/logger.ts
+++ utils/logger.ts
@@ -1,29 +1,28 @@
-const logLevel: number = parseInt(process.env.PLASMO_PUBLIC_LOG_LEVEL || "1")
-
 export class Logger {
-  static #appName = "NCOverlayHelper"
+  static appName = "NCOverlayHelper"
+  static logLevel = parseInt(process.env.PLASMO_PUBLIC_LOG_LEVEL || "1")
 
   private static getHeader() {
-    return `[${this.#appName}]`
+    return `[${this.appName}]`
   }
 
   static debug(message: string, ...args: unknown[]) {
-    if (logLevel > 0) return
+    if (this.logLevel > 0) return
     console.log(this.getHeader(), message, ...args)
   }
 
   static info(message: string, ...args: unknown[]) {
-    if (logLevel > 1) return
+    if (this.logLevel > 1) return
     console.info(this.getHeader(), message, ...args)
   }
 
   static warn(message: string, ...args: unknown[]) {
-    if (logLevel > 2) return
+    if (this.logLevel > 2) return
     console.warn(this.getHeader(), message, ...args)
   }
 
   static error(message: string, ...args: unknown[]) {
-    if (logLevel > 3) return
+    if (this.logLevel > 3) return
     console.error(this.getHeader(), message, ...args)
   }
 }
