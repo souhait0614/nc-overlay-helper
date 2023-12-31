f85b347b640dc64c2828a077b2c5828162dbedc9
すえ <souhait.0614@gmail.com>
2023-12-31 17:38:14 +0900

    useStorageを自作

--- hooks/useSettings.ts
+++ hooks/useSettings.ts
@@ -1,38 +1,35 @@
-import { useStorage } from "@plasmohq/storage/hook"
 import deepmerge from "deepmerge"
 import { useCallback, useMemo } from "react"
 import { defaultSettings } from "types/settings"
 
+import { useStorage } from "./useStorage"
+
 import type { Settings } from "types/settings"
 
 export const useSettings = () => {
-  const [storageSettings, setStorageSettings] = useStorage<
-    Settings | undefined
-  >("settings", undefined)
+  const { data, setData, loading } = useStorage<Settings>("settings")
 
   const settings = useMemo(
-    () =>
-      storageSettings ? deepmerge(defaultSettings, storageSettings) : undefined,
-    [storageSettings]
+    () => (!loading ? deepmerge(defaultSettings, data ?? {}) : undefined),
+    [loading, data]
   )
   const setSetting = useCallback(
     (key: keyof Settings, value: Settings[typeof key]) => {
       if (settings)
-        setStorageSettings({
+        setData({
           ...settings,
           [key]: value
         })
-      else {
-        setStorageSettings({
+      else
+        setData({
           ...defaultSettings,
           [key]: value
         })
-      }
     },
-    [setStorageSettings, settings]
+    [setData, settings]
   )
   const resetSettings = useCallback(() => {
-    setStorageSettings(defaultSettings)
-  }, [setStorageSettings])
+    setData(undefined)
+  }, [setData])
   return [settings, setSetting, resetSettings] as const
 }
---
+++ hooks/useStorage.ts
@@ -0,0 +1,73 @@
+import { Storage } from "@plasmohq/storage"
+import { useEffect, useMemo, useState, useCallback } from "react"
+
+import type { StorageCallbackMap } from "@plasmohq/storage"
+import type { Dispatch, SetStateAction } from "react"
+
+type SetDataDispatch<T> = Dispatch<SetStateAction<T | undefined>>
+
+export type UseStorageReturnValues<T> =
+  | {
+      data: T | undefined
+      setData: SetDataDispatch<T>
+      loading: false
+    }
+  | {
+      data: undefined
+      setData: SetDataDispatch<T>
+      loading: true
+    }
+export const useStorage = <T>(key: string): UseStorageReturnValues<T> => {
+  const [data, setData] = useState<T | undefined>(undefined)
+  const [loading, setLoading] = useState(true)
+  const storage = useMemo(() => new Storage(), [])
+
+  useEffect(() => {
+    console.log(data, loading)
+  }, [data, loading])
+
+  const setStorageData = useCallback<SetDataDispatch<T>>(
+    (setStateAction) => {
+      if (typeof setStateAction === "function") {
+        const result = (
+          setStateAction as (prevState: T | undefined) => T | undefined
+        )(data)
+        setData(result)
+        storage.set(key, result)
+      } else {
+        setData(setStateAction)
+        storage.set(key, setStateAction)
+      }
+    },
+    [data, key, storage]
+  )
+
+  useEffect(() => {
+    const callbackMap: StorageCallbackMap = {
+      [key]: ({ newValue }) => {
+        setData(newValue)
+        setLoading(false)
+      }
+    }
+    storage.watch(callbackMap)
+    storage.get<T>(key).then((value) => {
+      setData(value)
+      setLoading(false)
+    })
+    return () => {
+      storage.unwatch(callbackMap)
+    }
+  }, [key, storage])
+
+  return loading
+    ? {
+        data: undefined,
+        setData: setStorageData,
+        loading
+      }
+    : {
+        data,
+        setData: setStorageData,
+        loading
+      }
+}
