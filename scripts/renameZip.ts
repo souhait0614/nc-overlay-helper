5241ac3c0b9645be5370a6c50bc39a94389fb045
すえ <souhait.0614@gmail.com>
2024-01-14 00:25:01 +0900

    build済みzipファイルをリネームするように変更

---
+++ scripts/renameZip.ts
@@ -0,0 +1,16 @@
+import { rename } from "fs/promises"
+import { join } from "path"
+
+import { displayName, version } from "package.json"
+
+const zipFileNames = {
+  chrome: "chrome-mv3-prod.zip",
+  filefox: "firefox-mv3-prod.zip"
+} as const
+
+const outputDir = "./build"
+
+Object.entries(zipFileNames).map(async ([name, fileName]) => {
+  const newFileName = `${displayName}_v${version}-${name}.zip`
+  await rename(join(outputDir, fileName), join(outputDir, newFileName))
+})
